e00a79a69a7dd273501dd8a9022afb6793c7ae36
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-02-28 09:38:34 +0800

    Assignment101: Madlibs Game

---
+++ Main.py
@@ -0,0 +1,11 @@
+place1 = input("Enter a place: ")
+animal1 = input("Enter an animal: ")
+adjective1 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb (action): ")
+noun1 = input("Enter a noun (thing): ")
+name1 = input("Enter a name: ")
+
+# Creating the Mad Libs story
+print(f"\nOne day, {name1} went to the {place1}.")
+print(f"There, {name1} saw a {adjective1} {animal1} {verb1} near a {noun1}.")
+print(f"{name1} was amazed and decided to take a picture of the {animal1}.")
